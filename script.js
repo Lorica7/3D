44bc6b3400fe4d88bfc25795efcc5dcd8be26a30
Lorica7 <38511443+Lorica7@users.noreply.github.com>
2021-08-22 16:52:06 -0400

    visibility change

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const showCode = document.querySelector(".non-mobile");
 const dir = document.querySelector(".computer-directions");
 
 if (!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent))) {
-    showCode.classList.add("overlay")
+    showCode.classList.add("overlay");
+    showCode.style.visibility = "visible"
     dir.style.visibility = "visible"
 }
