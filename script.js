cfbaaf941966f64a6f6ca73868d94cb17e0d95d0
Lorica7 <38511443+Lorica7@users.noreply.github.com>
2021-08-22 16:42:38 -0400

    display conditions for diff devices

--- script.js
+++ script.js
@@ -2,8 +2,11 @@
 
 when hover or click picture the QR code appears as overlay */
 
-const showCode = document.querySelector(".non-mobile")
+const showCode = document.querySelector(".non-mobile");
+
+const dir = document.querySelector(".computer-directions");
 
 if (!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent))) {
     showCode.classList.add("overlay")
+    dir.style.visibility = "visible"
 }
